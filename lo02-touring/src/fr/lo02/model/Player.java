6cca5cc0c67c037d22cfbfe8ea8866a4a89f09d7
Kevin <kevin.maciolek@gmail.com>
2012-12-05 15:29:15 +0100

    Hand add in player

--- lo02-touring/src/fr/lo02/model/Player.java
+++ lo02-touring/src/fr/lo02/model/Player.java
@@ -1,13 +1,13 @@
 package fr.lo02.model;
 
-import fr.lo02.model.stack.PlayerStack;
+import fr.lo02.model.stack.Hand;
 
 public class Player {
 	
 	private String name;
 	private int color;
 	
-	PlayerStack myStack = new PlayerStack();
+	Hand hand = new Hand();
 	
 	public Player(String name, int color) {
 		this.name = name;
